ffc8384a622f854c41428c438d753d5b1149e5a8
Bobby Lee <hugecock@penis.com>
2025-09-08 19:01:35 +0300

    add Example 005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("What's your name?");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "bob")
+{
+    Console.WriteLine("Hello, sweetheart!");
+}
+
+else
+{
+    Console.Write(username);
+    Console.Write(", what do you need?");
+}
